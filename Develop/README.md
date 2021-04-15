cef642c41825de2599ddae67595fbdb15f3279a6
Linegrind <linegrind@icloud.com>
2021-04-15 13:03:06 -0600

    generated readme, and called the prompt function

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+Pro RDME Generator
+    
+    #### Table of Contents
+
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Test Instructions](#test-instructions)
+6. [License](#license)
+7. [Questions](#questions)
+
+## Project Description
+* Making a readme
+
+## Installation Instructions
+* Inquirer
+
+## Usage Information
+* To make a quality readme for an application
+
+## Contributor Guidelines
+* No
+
+## Test Instructions
+* Inquirer must be installed onto the local terminal. Then enter node index into the terminal to start the questions.
+
+## License
+* licensed under the [MIT License](LICENSE.txt)
+
+## Questions
+* For additional help or questions about collaboration, please reach out to linegrind@icloud.com
+* Follow me on Github at [linegrind675](http://github.com/linegrind675)
--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
+
     return inquirer.prompt([
+
         {
             type: "input",
             name: "project_title",
@@ -53,5 +56,55 @@ function promptUser() {
             name: "github",
             message: "Enter your GitHub username."
         }
-    ])
+    ]);
 }
+
+function generateREADME(answers) {
+    return `${answers.project_title}
+    
+    #### Table of Contents
+
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Test Instructions](#test-instructions)
+6. [License](#license)
+7. [Questions](#questions)
+
+## Project Description
+* ${answers.description}
+
+## Installation Instructions
+* ${answers.install}
+
+## Usage Information
+* ${answers.use}
+
+## Contributor Guidelines
+* ${answers.contributions}
+
+## Test Instructions
+* ${answers.test}
+
+## License
+* licensed under the ${answers.license}
+
+## Questions
+* For additional help or questions about collaboration, please reach out to ${answers.email}
+* Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+
+}
+
+promptUser()
+    .then(function(answers) {
+        const readme = generateREADME(answers);
+
+        return writeFileAsync("README.md", readme);
+    })
+    .then(function() {
+        console.log(" README.md has been created");
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
